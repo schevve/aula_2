f8e11cd7a150c13a9fdc17420031c897175de40f
schevve <samuel.scardoso2004@gmail.com>
2026-02-26 21:24:37 -0300

    feat: informações do sistema

--- src/main/java/com/aula2/demo/controller/AtividadeController.java
+++ src/main/java/com/aula2/demo/controller/AtividadeController.java
@@ -1,11 +1,14 @@
 package com.aula2.demo.controller;
 
+import java.time.LocalDateTime;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.aula2.demo.service.AtividadeService;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
 @RestController
@@ -24,6 +27,17 @@ public class AtividadeController {
     @GetMapping("/soma/{n1}/{n2}")
     public String soma(@PathVariable int n1, @PathVariable int n2) {
         return String.format("%d", atividadeService.soma(n1, n2));
+    }
+
+    @GetMapping("/info")
+    public String info() {
+        String appName = "Aula_2";
+        String javaVersion = System.getProperty("java.version");
+        LocalDateTime now = LocalDateTime.now();
+        String msg = "O sistema está funcional.";
+        return String.format(
+                "Nome do sistema: %s   \nVersão do Java: %s   \nData e hora atuais: %s   \n%s",
+                appName, javaVersion, now, msg);
     }
 
 }
