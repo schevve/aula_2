91b729afafdbcf38640d0591365ac067b4295be3
schevve <samuel.scardoso2004@gmail.com>
2026-03-05 20:25:18 -0300

    feat: entry point

---
+++ src/main/java/com/aula2/sistemaestudantes/SistemaestudantesApplication.java
@@ -0,0 +1,13 @@
+package com.aula2.sistemaestudantes;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaestudantesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaestudantesApplication.class, args);
+	}
+}
